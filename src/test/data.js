baa71095ee64d0661a6efb1f0115c97fdd00cd20
vragosta <vincentpasqualeragosta@gmail.com>
2024-02-26 01:04:58 -0500

    create test js file to prevent muck in front page vue file

---
+++ src/test/data.js
@@ -0,0 +1,49 @@
+export default function data() {
+    return {
+        heroVideoTrigger: null,
+        projectCollection: [
+            {
+                title: '10glo',
+                url: '#',
+                image: 'https://devanimecards.test/wp-content/uploads/2024/01/10glo.png'
+            },
+            {
+                title: 'AMASafety',
+                url: '#',
+                image: 'https://devanimecards.test/wp-content/uploads/2024/01/amasafety.jpeg'
+            },
+            {
+                title: 'Analytic Partners',
+                url: '#',
+                image: 'https://devanimecards.test/wp-content/uploads/2024/01/analytic-partners.jpeg'
+            },
+            {
+                title: 'Bershlaw',
+                url: '#',
+                image: 'https://devanimecards.test/wp-content/uploads/2024/01/bershlaw.jpeg'
+            },
+            {
+                title: 'Holiday Afterpay',
+                url: '#',
+                image: 'https://devanimecards.test/wp-content/uploads/2024/01/holiday-afterpay.jpeg'
+            },
+            {
+                title: 'IRC Events',
+                url: '#',
+                image: 'https://devanimecards.test/wp-content/uploads/2024/01/irc-events-scaled.jpeg'
+            }
+        ],
+        highlights: [
+            {
+                title: 'Test Heading A',
+                excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat.',
+                url: '#'
+            },
+            {
+                title: 'Test Heading B',
+                excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat.',
+                url: '#'
+            }
+        ]
+    }
+}
