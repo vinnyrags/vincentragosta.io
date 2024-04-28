69a7a4ce9ddeddb9af0e8731b70ac22c58d16959
vinnyrags <vincentpasqualeragosta@gmail.com>
2024-04-27 21:29:16 -0400

    fix scss files in accordance to stylelint

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new StyleLintPlugin({
+        files: "src/**/**/**/**/*.scss",
+      }),
+    ],
+  },
 });
