d727aa1d2307af6b241a4927ed189ecd291d3686
vinnyrags <vincentpasqualeragosta@gmail.com>
2025-05-20 00:15:35 -0400

    temp hide header + remove auto title in page template + clean up hero render and save.js + start styling hero block

--- wp-content/themes/vincentragosta/blocks/hero/save.js
+++ wp-content/themes/vincentragosta/blocks/hero/save.js
@@ -5,31 +5,31 @@ import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 // relying on the server-side render_callback for the full frontend output.
 export default function save() {
     // Apply block props for save. This adds necessary attributes to the outer div.
-    const blockProps = useBlockProps.save();
+    // const blockProps = useBlockProps.save();
 
     // The structure below should mirror the HTML wrapper elements
     // that surround the InnerBlocks output in your render_callback (vincentragosta_render_hero_block).
     // We do NOT include dynamic attributes like title, subtitle, or svg here,
     // as those are handled by the PHP render function.
     return (
-        // This corresponds to the main wrapper div in render_callback
-        <div {...blockProps}>
-            {/* This corresponds to the '.hero-block__content' div in render_callback */}
-            <div className="hero__content">
-                {/* This corresponds to the '.hero__links' div in render_callback
-                    where the buttons are placed. */}
-                <div className="hero__links">
-                    {/* InnerBlocks.Content outputs the saved markup of the inner blocks (core/buttons).
-                        This is essential for the editor to find and load the inner blocks on reload. */}
+        // // This corresponds to the main wrapper div in render_callback
+        // <div {...blockProps}>
+        //     {/* This corresponds to the '.hero-block__content' div in render_callback */}
+        //     <div className="hero__content">
+        //         {/* This corresponds to the '.hero__links' div in render_callback
+        //             where the buttons are placed. */}
+        //         <div className="hero__links">
+                    /* InnerBlocks.Content outputs the saved markup of the inner blocks (core/buttons).
+                        This is essential for the editor to find and load the inner blocks on reload. */
                     <InnerBlocks.Content />
-                </div>
-                {/* We don't include the InnerBlocks.Content for the SVG side,
-                    as the SVG is not an InnerBlock managed by this parent block. */}
-            </div>
-            {/* This corresponds to the '.hero__svg' div in render_callback */}
-            <div className="hero__svg">
-                {/* SVG content is handled by the PHP render_callback based on the svgAsset attribute */}
-            </div>
-        </div>
+        //         </div>
+        //         {/* We don't include the InnerBlocks.Content for the SVG side,
+        //             as the SVG is not an InnerBlock managed by this parent block. */}
+        //     </div>
+        //     {/* This corresponds to the '.hero__svg' div in render_callback */}
+        //     <div className="hero__svg">
+        //         {/* SVG content is handled by the PHP render_callback based on the svgAsset attribute */}
+        //     </div>
+        // </div>
     );
 }
