dcab237678790c7056be051f88cc484c53bb7855
vinnyrags <vincentpasqualeragosta@gmail.com>
2025-09-02 21:37:50 -0400

    shutter card active / inactive states

--- wp-content/themes/vincentragosta/blocks/shutter-cards/render.php
+++ wp-content/themes/vincentragosta/blocks/shutter-cards/render.php
@@ -1,36 +1,36 @@
 <?php
 /**
  * Server-side rendering for the Shutter Cards container block.
- *
- * Expected:
- * @var array    $attributes
- * @var string   $content
- * @var WP_Block $block
  */
 
-$card_count = 0;
-if (isset($block) && !empty($block->inner_blocks)) {
-    $card_count = count($block->inner_blocks);
-}
-
-$modified_content = $content;
-
-// If there are two or more cards, append the 'shutter-card--inactive' class.
-if ($card_count >= 2) {
-    // Define what we're looking for.
-    $search = 'wp-block-vincentragosta-shutter-card';
-
-    // Define what we'll replace it with (the original class plus our new inactive class).
-    $replace = 'wp-block-vincentragosta-shutter-card shutter-card--inactive';
-
-    // Perform the replacement on the rendered inner block content.
-    $modified_content = str_replace($search, $replace, $content);
-}
-
 $wrapper_attributes = get_block_wrapper_attributes();
+$inner_blocks = isset($block) ? $block->inner_blocks : [];
+$card_count = count($inner_blocks);
+$container_styles = '';
+
+// If we have more than one card, calculate the inactive width and set it as a CSS variable.
+if ($card_count > 1) {
+    $inactive_width_percentage = 60 / $card_count;
+    $container_styles = sprintf('style="--card-inactive-width: %f%%;"', $inactive_width_percentage);
+}
 ?>
 <div <?php echo $wrapper_attributes; ?>>
-    <div class="shutter-cards-container">
-        <?php echo $modified_content; ?>
+    <div class="shutter-cards-container" <?php echo $container_styles; ?>>
+        <?php if (!empty($inner_blocks)) : ?>
+            <?php foreach ($inner_blocks as $index => $inner_block) : ?>
+                <?php
+                $inner_block_content = $inner_block->render();
+
+                // Add the 'inactive' class to all cards except the first one.
+                if ($card_count >= 2 && $index > 0) {
+                    $search = 'wp-block-vincentragosta-shutter-card';
+                    $replace = 'wp-block-vincentragosta-shutter-card shutter-card--inactive';
+                    echo str_replace($search, $replace, $inner_block_content);
+                } else {
+                    echo $inner_block_content;
+                }
+                ?>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </div>
 </div>
