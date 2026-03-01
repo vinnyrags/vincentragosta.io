9fe83f1a48870fb701e62e485972ef17dba0f605
vinnyrags <vincentpasqualeragosta@gmail.com>
2026-03-01 00:18:38 -0500

    add lists to scroll animation

--- wp-content/themes/child-theme/src/Providers/Theme/assets/js/scroll-animation.js
+++ wp-content/themes/child-theme/src/Providers/Theme/assets/js/scroll-animation.js
@@ -8,6 +8,7 @@
     const selectors = [
         'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
         '.site-main p',
+        '.site-main ul', '.site-main ol', '.site-main dl',
         '.site-main .wp-block-image',
         '.site-main .wp-block-button',
         '.site-main .wp-block-buttons',
