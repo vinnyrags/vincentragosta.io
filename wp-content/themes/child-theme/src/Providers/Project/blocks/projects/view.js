d197ef95dfdb369baad25b5cba9bbef8d17437a0
vinnyrags <vincentpasqualeragosta@gmail.com>
2026-03-16 13:44:20 -0400

    fix project css regression (widths)

--- wp-content/themes/child-theme/src/Providers/Project/blocks/projects/view.js
+++ wp-content/themes/child-theme/src/Providers/Project/blocks/projects/view.js
@@ -109,7 +109,7 @@ function preselectDropdownItem(dropdown, item) {
  * Initialize projects block functionality
  */
 export function initProjects() {
-    const blocks = document.querySelectorAll('.wp-block-child-theme-projects');
+    const blocks = document.querySelectorAll('.wp-block-parent-theme-projects');
     if (!blocks.length) return;
 
     blocks.forEach((block) => {
